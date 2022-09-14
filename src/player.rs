d314eeea3fa34e2d8f88eb06ab94e1013e2f7e8a
Martin Penckert <martin.penckert@gmail.com>
2022-09-14 21:33:53 +0200

    Adding Camera and Graphix

--- src/player.rs
+++ src/player.rs
@@ -1,4 +1,4 @@
-use crate::prelude::*;
+use crate::{prelude::*, camera};
 
 pub struct Player {
     pos: Point,
@@ -9,11 +9,18 @@ impl Player {
         Player { pos }
     }
 
-    pub fn render(&self, ctx: &mut BTerm) {
-        ctx.set(self.pos.x, self.pos.y, WHITE, BLACK, to_cp437('@'));
+    pub fn render(&self, ctx: &mut BTerm, camera: &Camera) {
+        ctx.set_active_console(1);
+        ctx.set(
+            self.pos.x - camera.left_x, 
+            self.pos.y - camera.top_y, 
+            WHITE, 
+            BLACK, 
+            to_cp437('@')
+        );
     }
 
-    pub fn update(&mut self, ctx: &mut BTerm, map: &Map) {
+    pub fn update(&mut self, ctx: &mut BTerm, map: &Map, camera: &mut Camera) {
         if let Some(key) = ctx.key {
             let delta = match key {
                 VirtualKeyCode::Left => Point::new(-1, 0),
@@ -26,6 +33,7 @@ impl Player {
             let new_pos = self.pos + delta;
             if map.can_enter_tile(new_pos) {
                 self.pos = new_pos;
+                camera.on_player_move(new_pos);
             }
         }
     }
