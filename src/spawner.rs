61b84fdf2717ad3b9da43a29a4f786ec24a57001
Martin Penckert <martin.penckert@gmail.com>
2022-09-15 16:40:15 +0200

    Change arch to ECS

---
+++ src/spawner.rs
@@ -0,0 +1,17 @@
+// spawner.rs
+// Module to handle spawning entities.
+
+use crate::prelude::*;
+
+pub fn spawn_player(ecs: &mut World, pos: Point) {
+    ecs.push(
+        (
+            Player, 
+            pos, 
+            Render {
+                color: ColorPair::new(WHITE, BLACK), 
+                glyph: to_cp437('@')
+            }
+        )
+    );
+}
