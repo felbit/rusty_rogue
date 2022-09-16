7def7641ec31682ffbc2f81c09165647f49201cd
Martin Penckert <martin.penckert@gmail.com>
2022-09-16 19:54:16 +0200

    Adding enemy movement - randomly for now

--- src/systems.rs
+++ src/systems.rs
@@ -2,12 +2,15 @@ mod collision;
 mod entity_render;
 mod map_render;
 mod player_input;
+mod random_move;
 
 pub fn build_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(player_input::player_input_system())
         .add_system(collision::collision_system())
+        .flush() // apply CommandBuffer changes immediately
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
+        .add_system(random_move::random_move_system())
         .build()
 }
